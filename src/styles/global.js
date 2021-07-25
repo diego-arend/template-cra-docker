3ef3f0e1f95f5e535459ac1127298333ef5074c7
Diego Arend <d.arend@paylivre.com>
2021-07-25 15:30:52 -0300

    Instalação dependencias Styled Components e ajuste do código

---
+++ src/styles/global.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+* {
+    box-sizing: border-box
+}
+
+html {
+    font-size: 62.5% // rem
+}
+
+body {
+    margin: 0px;
+    padding: 0px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`;
+
+export default GlobalStyles;
