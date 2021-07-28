bef420a968ec4404ea547bc6e2b08815c6e88438
Diego Arend <d.arend@paylivre.com>
2021-07-28 14:37:46 -0300

    wip

--- src/browserRouter.js
+++ src/browserRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, Switch, Redirect } from 'react-router'
+import { Router, Route, Switch } from 'react-router'
 import Main from './pages/Main'
 import UserInfo from './pages/UserInfo'
 import NotFound from './pages/NotFound'
